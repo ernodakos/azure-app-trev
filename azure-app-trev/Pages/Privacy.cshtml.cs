c3e82bb4b95fbae580ab8463e9bbc86fccd088d4
ernodakos <57793198+ernodakos@users.noreply.github.com>
2026-02-03 19:25:10 +0100

    Add greetings

--- azure-app-trev/Pages/Privacy.cshtml.cs
+++ azure-app-trev/Pages/Privacy.cshtml.cs
@@ -1,19 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace azure_app_trev.Pages
 {
     public class PrivacyModel : PageModel
     {
         private readonly ILogger<PrivacyModel> _logger;
+        private readonly IConfiguration _configuration;
 
-        public PrivacyModel(ILogger<PrivacyModel> logger)
+        public PrivacyModel(ILogger<PrivacyModel> logger, IConfiguration configuration)
         {
             _logger = logger;
+            this._configuration = configuration;
         }
 
         public void OnGet()
         {
+            ViewData["Greetings"] = _configuration["Greetings"];
         }
     }
 
